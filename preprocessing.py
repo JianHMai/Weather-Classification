af4683bf0ba37a3254c1feb7a410d3760f363b74
Jian Mai <42229566+JianHMai@users.noreply.github.com>
2022-04-02 19:00:29 -0400

    Add data normalization

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,28 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 import os
 import glob 
+import cv2 as cv
 
 # Retrieve current directory
 cwd = os.getcwd() + '\\dataset\\'
 root_dir = os.path.dirname(cwd)
 
+# Normalize images
+for data in os.listdir(cwd):
+    directory = os.path.join(root_dir, data, '*')
+    directory2 = glob.glob(directory)
+    for img in directory2:
+        print(img)
+        # Read image
+        pic = cv.imread(img)
+        # Resize new image to 100x100
+        pic = cv.resize(pic, (100,100))
+        # Normalize images
+        cv.normalize(pic, pic, 0, 225, cv.NORM_MINMAX)
+        # Override iamges
+        cv.imwrite(img, pic) 
+
+# Used for data argumentation 
 for data in os.listdir(cwd):
     directory = os.path.join(root_dir, data, '*')
     directory2 = glob.glob(directory)
